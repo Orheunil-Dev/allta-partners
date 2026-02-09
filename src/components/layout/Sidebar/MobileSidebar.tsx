135fcce2d0181fb60c445b6b89a2f13716ad4fcf
shoh <shoh@orheun.com>
2026-02-09 13:56:31 +0900

    fix: 빌드 에러 수정

--- src/components/layout/Sidebar/MobileSidebar.tsx
+++ src/components/layout/Sidebar/MobileSidebar.tsx
@@ -51,54 +51,6 @@ export const MobileSidebar = () => {
           <Image src={hamburgerIcon} alt="메뉴" className="w-[40px] h-[40px]" />
         </button>
       </div>
-
-      <div className="flex flex-col px-[10px] pt-[20px] pb-[40px] gap-y-[8px] text-white text-[16px] font-semibold overflow-y-auto">
-        {menuItems.map((item, index) => (
-          <div key={index}>
-            <div
-              onClick={() => {
-                item.route && router.push(item.route);
-                setIsOpen(false);
-              }}
-              className={`flex items-center w-full h-[40px] px-[12px] rounded-[12px] ${
-                isCurrentPathname(item) ? `bg-white/20` : `bg-transparent `
-              } 
-            ${!item.route || (!isCurrentPathname(item) && " hover:bg-white/10")}
-            ${item.route ? `cursor-pointer` : `cursor-default`}`}
-            >
-              <Image
-                src={item.icon!}
-                alt={item.category}
-                className="w-[20px] h-[20px] mr-[8px]"
-              />
-              <p className="text-[16px]">{item.category}</p>
-            </div>
-
-            <div
-              className={`flex flex-col ${
-                item.menus && `pt-[8px]`
-              }  gap-y-[8px]`}
-            >
-              {item.menus?.map((menu) => (
-                <div
-                  key={menu.name}
-                  className={`flex items-center w-full h-[40px] pl-[44px] gap-x-[10px] rounded-[12px] cursor-pointer ${
-                    isCurrentRoute(menu.route)
-                      ? `bg-white/20`
-                      : `bg-transparent hover:bg-white/10`
-                  }`}
-                  onClick={() => {
-                    setIsOpen(false);
-                    router.push(menu.route);
-                  }}
-                >
-                  <p className="text-[16px]">{menu.name}</p>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
